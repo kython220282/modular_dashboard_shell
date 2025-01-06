5c8b7fc6606a5a0475e6706dd155cd82ab5e2118
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2025-01-06 14:01:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,14 @@ import streamlit as st
 st.set_page_config(page_title="Modular Dashboard", page_icon = ":bar_chart:",layout="wide", initial_sidebar_state="auto", menu_items=None)
 st.title('<Business Name> Performance Dashboard')
 def main():
-  tab1, tab2, tab3 = st.tabs(["Overall Performance","Metrics 1","Metrics 2"])
+  tab1, tab2, tab3, tab4 = st.tabs(["Upload Data","Overall Performance","Metrics 1","Metrics 2"])
   with tab1:
-    st.subheader("Overall Performance")
-    
+    st.subheader("Upload Data")
   with tab2:
-    st.subheader("Performance on <Metrics 1>")
-
+    st.subheader("Overall Performance")
   with tab3:
+    st.subheader("Performance on <Metrics 1>")
+  with tab4:
     st.subheader("Performance on <Metrics 2>")
 
 
