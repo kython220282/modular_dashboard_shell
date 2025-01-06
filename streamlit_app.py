7ac679f0726765e68ff2ca74dcacf61035a474ab
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2025-01-06 17:05:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,10 @@ def main():
   tab1, tab2, tab3, tab4 = st.tabs(["Upload Data","Overall Performance","Metrics 1","Metrics 2"])
   with tab1:
     st.write("Upload Data")
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+      dataframe = pd.read_csv(uploaded_file)
+      st.write(dataframe)
   with tab2:
     st.write("Overall Performance")
   with tab3:
