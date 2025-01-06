11fabbe8d750b3817e9dfae590c164fac9264aba
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2025-01-06 11:24:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.set_page_config(page_title="Modular Dashboard", page_icon = ":bar_chart:",layout="wide", initial_sidebar_state="auto", menu_items=None)
-st.title('Application Name')
+st.title('<Business Name> Performane Dashboard')
 st.write('--------')
 def main():
   tab1, tab2, tab3 = st.tabs(["Overall Performance","Metrics 1","Metrics 2"])
