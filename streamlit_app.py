27681f8c4e88793b201b91ab68d1b20d654810ad
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2025-01-06 11:13:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.set_page_config(page_title="Modular Dashboard", page_icon = ":chart:",layout="wide", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Modular Dashboard", page_icon = ":bar_chart:",layout="wide", initial_sidebar_state="auto", menu_items=None)
 st.title('Application Name')
 st.write('--------')
