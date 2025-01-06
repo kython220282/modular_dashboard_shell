eab2d37994573097cfcd2833693d2aa34e572317
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2025-01-06 14:38:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,16 @@ st.title('<Business Name> Performance Dashboard')
 def main():
   tab1, tab2, tab3, tab4 = st.tabs(["Upload Data","Overall Performance","Metrics 1","Metrics 2"])
   with tab1:
-    st.subheader("Upload Data")
+    col1, col2 = st.columns([1,3])
+    with col1:
+      with st.container():
+        uploaded_file = st.file_uploader("Choose a file")
+        if uploaded_file is not None:
+          dataframe = pd.read_csv(uploaded_file)
+          st.write(dataframe)
+    with col1:
+      with st.container():
+        st.subheader("Review Data uploaded")
   with tab2:
     st.subheader("Overall Performance")
   with tab3:
